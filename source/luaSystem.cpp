cd5319a089ce384c37e555f17457b4fe31f31ed1
Rinnegatamante <rinnegatamante@gmail.com>
2016-05-03 18:39:29 +0200

    Added some funcs in luaSystem.
    Added some funcs in luaSystem.

--- source/luaSystem.cpp
+++ source/luaSystem.cpp
@@ -31,14 +31,20 @@
 #- Special thanks to Aurelio for testing, bug-fixing and various help with codes and implementations -------------------#
 #-----------------------------------------------------------------------------------------------------------------------*/
 
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include <SDL/SDL_opengl.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include "include/luaplayer.h"
-
+#include "include/graphics/graphics.h"
 #define stringify(str) #str
 #define VariableRegister(lua, value) do { lua_pushinteger(lua, value); lua_setglobal (lua, stringify(value)); } while(0)
 #define MAX_RAM_ALLOCATION	10485760
+#define u8 uint8_t
+#define u32 uint32_t
 
 static int lua_exit(lua_State *L)
 {
@@ -46,14 +52,104 @@ static int lua_exit(lua_State *L)
 	#ifndef SKIP_ERROR_HANDLING
 		if (argc != 0) return luaL_error(L, "wrong number of arguments");
 	#endif
+	drawCommand("System.exit(): ", "Closing interpreter.\n");
 	char string[20];
 	strcpy(string,"lpp_exit_0456432");
 	luaL_dostring(L, "collectgarbage()");
 	return luaL_error(L, string); // NOTE: This is a fake error
 }
 
+static int lua_launch(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	#ifndef SKIP_ERROR_HANDLING
+		if (argc != 2) return luaL_error(L, "wrong number of arguments");
+	#endif
+	const char* path = luaL_checkstring(L, 1);
+	u8 mediatype = luaL_checkinteger(L, 2);
+	drawCommand("System.launch3DSX: ", "Closing interpreter.\n");
+	char string[20];
+	strcpy(string,"lpp_exit_0456432");
+	luaL_dostring(L, "collectgarbage()");
+	return luaL_error(L, string); // NOTE: This is a fake error
+}
+
+static int lua_launchcia(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	#ifndef SKIP_ERROR_HANDLING
+		if (argc != 2) return luaL_error(L, "wrong number of arguments");
+	#endif
+	u32 unique_id = luaL_checkinteger(L,1);
+	u32 mediatype = luaL_checkinteger(L,2);
+	drawCommand("System.launchCIA: ", "Closing interpreter.\n");
+	char string[20];
+	strcpy(string,"lpp_exit_0456432");
+	luaL_dostring(L, "collectgarbage()");
+	return luaL_error(L, string); // NOTE: This is a fake error
+}
+
+static int lua_launcharm9(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	#ifndef SKIP_ERROR_HANDLING
+		if (argc != 2) return luaL_error(L, "wrong number of arguments");
+	#endif
+	const char* file = luaL_checkstring(L, 1);
+	u32 offset = luaL_checkinteger(L, 2);
+	drawCommand("System.launchPayload: ", "Closing interpreter.\n");
+	char string[20];
+	strcpy(string,"lpp_exit_0456432");
+	luaL_dostring(L, "collectgarbage()");
+	return luaL_error(L, string); // NOTE: This is a fake error
+}
+
+static int lua_installcia(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	#ifndef SKIP_ERROR_HANDLING
+		if (argc != 2) return luaL_error(L, "wrong number of arguments");
+	#endif
+	const char* path = luaL_checkstring(L, 1);
+	u8 mediatype = luaL_checkinteger(L, 2);
+	drawWarning("Warning: ", "Skipping System.installCia function.\n");
+	return 0;
+}
+
+static int lua_getFW(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	#ifndef SKIP_ERROR_HANDLING
+		if (argc != 0) return luaL_error(L, "wrong number of arguments");
+	#endif
+	drawCommand("System.getFirmware: ", "Returning 2.50-9 FIRM version.\n");
+	lua_pushinteger(L,2);
+	lua_pushinteger(L,50);
+	lua_pushinteger(L,9);
+	return 3;
+}
+
+static int lua_getK(lua_State *L)
+{
+	int argc = lua_gettop(L);
+	#ifndef SKIP_ERROR_HANDLING
+		if (argc != 0) return luaL_error(L, "wrong number of arguments");
+	#endif
+	drawCommand("System.getKernel: ", "Returning 2.50-9 NATIVE_FIRM version.\n");
+	lua_pushinteger(L,2);
+	lua_pushinteger(L,50);
+	lua_pushinteger(L,9);
+	return 3;
+}
+
 //Register our System Functions
 static const luaL_Reg System_functions[] = {
+	{"getFirmware",			lua_getFW},
+	{"getKernel",			lua_getK},
+	{"launch3DSX",			lua_launch},
+	{"launchCIA",			lua_launchcia},
+	{"launchPayload",		lua_launcharm9},
+	{"installCIA",			lua_installcia},
 	{"exit",				lua_exit},
 	{0, 0}
 };
